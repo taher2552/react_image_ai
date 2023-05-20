db070b2e42576a09b1a4973e224c7c72112145fa
taher2552 <stardom.taher@gmail.com>
2023-05-20 13:31:04 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
-import React , { useState, useEffect } from 'react'
+import React , { useState, useEffect } from 'react';
+import { Card, FormFields, Loader } from '../component';
 
 const Home = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [allPost, setAllPost] = useState(null)
+
   return (
-<>
-    
-</>
+<section className='max-w-7xl mx-auto'>
+<div>
+    <h1 className='font-extrabold text-[#222328]'>The Community Showcase</h1>
+</div>
+
+</section>
   )
 }
 
